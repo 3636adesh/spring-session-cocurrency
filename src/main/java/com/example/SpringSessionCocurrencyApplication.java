06399af27192418118292852f4df7e21c86bddef
adeshmalunjkar <adesh.malunjkar@upgrad.com>
2025-01-20 15:41:04 +0530

    initial commit

---
+++ src/main/java/com/example/SpringSessionCocurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionCocurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSessionCocurrencyApplication.class, args);
+	}
+
+}
